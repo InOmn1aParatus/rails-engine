ff0cd1cc6fe09dc681dfef497568419261049db2
Matt Kragen <mattdkragen@gmail.com>
2021-09-05 13:59:23 -0700

    Finish first request specs for merchants and items

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -4,8 +4,26 @@ describe "Items API" do
   it "sends a list of items" do
     create_list(:item, 3)
 
-    get '/api/v1/items'
+    get '/api/v1/merchants/:merchant_id/items'
 
     expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items.count).to eq(3)
+    
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+    
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_an(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_an(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_an(Float)
+    end
   end
 end
