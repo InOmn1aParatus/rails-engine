163ffdd6766423d89a6102061738b7e4f259d5a2
Matt Kragen <mattdkragen@gmail.com>
2021-09-06 15:28:17 -0700

    Implement #create for Item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,6 +5,21 @@ module Api
         @items = Item.results(per_page, page_number)
         render json: ItemSerializer.new(@items)
       end
+      
+      def show
+        @item = Item.find(params[:id])
+        render json: ItemSerializer.new(@item)
+      end
+
+      def create
+        render json: Item.create(item_params)
+      end
+
+      private
+
+      def item_params
+        params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+      end
     end
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -23,7 +23,38 @@ describe 'Items API' do
           
           expect(item[:attributes]).to have_key(:unit_price)
           expect(item[:attributes][:unit_price]).to be_a(Numeric)
+          
+          expect(item[:attributes]).to have_key(:merchant_id)
+          expect(item[:attributes][:merchant_id]).to be_a(Numeric)
         end
+      end
+
+      it 'displays specific merchant' do
+        id = create(:item).id
+
+        get "/api/v1/items/#{id}"
+
+        item = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+
+        expect(item[:data]).to have_key(:id)
+        expect(item[:data][:id]).to be_a(String)
+
+        expect(item[:data]).to have_key(:attributes)
+        expect(item[:data][:attributes]).to be_a(Hash)
+
+        expect(item[:data][:attributes]).to have_key(:name)
+        expect(item[:data][:attributes][:name]).to be_a(String)
+        
+        expect(item[:data][:attributes]).to have_key(:description)
+        expect(item[:data][:attributes][:description]).to be_a(String)
+        
+        expect(item[:data][:attributes]).to have_key(:unit_price)
+        expect(item[:data][:attributes][:unit_price]).to be_a(Numeric)
+        
+        expect(item[:data][:attributes]).to have_key(:merchant_id)
+        expect(item[:data][:attributes][:merchant_id]).to be_a(Numeric)
       end
     end
 
@@ -62,6 +93,29 @@ describe 'Items API' do
         items = JSON.parse(response.body, symbolize_names: true)
 
         expect(items[:data].count).to eq(10)
+      end
+    end
+
+    describe 'RESTful actions' do
+      it "can create a new item" do
+        merchant_id = create(:merchant).id
+        
+        item_params = ({
+                        name: 'Banana',
+                        description: 'Quite banana',
+                        unit_price: 1.11,
+                        merchant_id: merchant_id
+                      })
+        headers = {"CONTENT_TYPE" => "application/json"}
+        
+        post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+        created_item = Item.last
+        
+        expect(response).to be_successful
+        expect(created_item.name).to eq(item_params[:name])
+        expect(created_item.description).to eq(item_params[:description])
+        expect(created_item.unit_price).to eq(item_params[:unit_price])
+        expect(created_item.merchant_id).to eq(item_params[:merchant_id])
       end
     end
   end
