0cfd7d32a7edce5cda9e52b7de9e34d2d90cdd95
Matt Kragen <mattdkragen@gmail.com>
2021-09-06 15:55:40 -0700

    Implement #update for item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -15,6 +15,10 @@ module Api
         render json: Item.create(item_params)
       end
 
+      def update
+        render json: Item.update(params[:id], item_params)
+      end
+
       private
 
       def item_params
