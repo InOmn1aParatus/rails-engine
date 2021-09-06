031abbfc4cb19b9602a97fbc819d91d90ab71bbd
Matt Kragen <mattdkragen@gmail.com>
2021-09-05 17:46:53 -0700

    Implement pagination and results per page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  def per_page
+    if params[:per_page].to_i.positive?
+      return params[:per_page].to_i 
+    else
+      20
+    end
+  end
+
+  def page_number
+    if params[:page].to_i >= 1
+      return params[:page].to_i
+    else
+      1
+    end
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.results(per_page, page_number)
+    if page_number == 1
+      limit(per_page)
+    else
+      limit(per_page).offset(per_page * (page_number - 1))
+    end
+  end
+  
 end
