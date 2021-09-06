bc5a27f929dc1df0247e3cc46e6dddc5b41f2cba
Matt Kragen <mattdkragen@gmail.com>
2021-09-05 19:16:16 -0700

    Move code into request spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,29 +1,74 @@
 require 'rails_helper'
 
 describe "Items API" do
-  it "sends a list of items" do
-    create_list(:item, 3)
-
-    get '/api/v1/merchants/:merchant_id/items'
-
-    expect(response).to be_successful
-
-    items = JSON.parse(response.body, symbolize_names: true)
-
-    expect(items.count).to eq(3)
+  context 'happy path' do
+    describe 'basic requests' do
+      it "sends a list of items" do
+        create_list(:item, 3)
     
-    items.each do |item|
-      expect(item).to have_key(:id)
-      expect(item[:id]).to be_an(Integer)
+        get '/api/v1/items'
     
-      expect(item).to have_key(:name)
-      expect(item[:name]).to be_an(String)
+        expect(response).to be_successful
+    
+        items = JSON.parse(response.body, symbolize_names: true)
+    
+        expect(items.count).to eq(3)
+        
+        items.each do |item|
+          expect(item).to have_key(:id)
+          expect(item[:id]).to be_an(Integer)
+        
+          expect(item).to have_key(:name)
+          expect(item[:name]).to be_an(String)
+    
+          expect(item).to have_key(:description)
+          expect(item[:description]).to be_an(String)
+    
+          expect(item).to have_key(:unit_price)
+          expect(item[:unit_price]).to be_an(Float)
+          
+          expect(item).to have_key(:merchant_id)
+          expect(item[:unit_price]).to be_an(Integer)
+        end
+      end
+    end
 
-      expect(item).to have_key(:description)
-      expect(item[:description]).to be_an(String)
+    describe 'optional query parameters' do
+      it 'displays default of 20 items per page' do
+        create_list(:merchant, 30)
 
-      expect(item).to have_key(:unit_price)
-      expect(item[:unit_price]).to be_an(Float)
+        get '/api/v1/merchants'
+
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants.count).to eq(20)
+      end
+      
+      it 'displays optional number of merchants per page' do
+        create_list(:merchant, 30)
+
+        get '/api/v1/merchants?per_page=15'
+
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants.count).to eq(15)
+      end
+
+      it 'can navigate to a specific page' do
+        create_list(:merchant, 30)
+
+        get '/api/v1/merchants?page=2'
+
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants.count).to eq(10)
+      end
     end
   end
 end
