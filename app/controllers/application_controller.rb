ebda4d56e0b9b32b7a6fec0de546886bbcb1152b
Matt Kragen <mattdkragen@gmail.com>
2021-09-06 20:44:54 -0700

    Begin work on custom exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def per_page
     if params[:per_page].to_i.positive?
       params[:per_page].to_i
@@ -14,4 +16,12 @@ class ApplicationController < ActionController::API
       1
     end
   end
+
+  private
+
+  # def record_not_found(exception)
+  #   render json: {error: exception.message}.to_json, status: 404
+  #   return
+  # end
+
 end
