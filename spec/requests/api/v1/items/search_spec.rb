18b5bc461d9e3ad023de65b80cdc8694a4024b6a
Matt Kragen <mattdkragen@gmail.com>
2021-09-07 19:22:49 -0700

    Set up testing outline for search function

---
+++ spec/requests/api/v1/items/search_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Item search' do
+  context 'happy path' do
+    describe 'find one result' do
+      it 'returns one item by case-sensitive search params' do
+    
+      end
+    end
+
+    describe 'find all results' do
+      it 'returns all items with matching string segment' do
+    
+      end
+    end
+
+    describe 'price search' do
+      it 'allows searching by min and max price' do
+      
+      end
+    end
+  end
+
+  context 'sad path' do
+    
+  end
+end
---
+++ spec/requests/api/v1/merchants/search_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant search' do
+  context 'happy path' do
+    describe 'find one result' do
+      it 'returns one merchant by case-sensitive search params' do
+    
+      end
+    end
+
+    describe 'find all results' do
+      it 'returns all merchants with matching string segment' do
+    
+      end
+    end
+  end
+
+  context 'sad path' do
+    
+  end
+end
