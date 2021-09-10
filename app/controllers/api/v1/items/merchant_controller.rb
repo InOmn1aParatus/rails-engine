237d8afe025b1bc80c0837056dd67abac96ef6c1
Matt Kragen <mattdkragen@gmail.com>
2021-09-09 23:03:08 -0700

    Correct method for test coverage

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -5,13 +5,7 @@ module Api
         before_action :set_item, only: [:show]
 
         def show
-          if @item
-            render json: MerchantSerializer.new(@item.merchant)
-          else
-            render json: MerchantSerializer.new(
-              Merchant.find(params[:merchant_id])
-            )
-          end
+          render json: MerchantSerializer.new(@item.merchant) if @item
         end
 
         private
